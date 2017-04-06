c0b55b640415832739dfe185ac2824e95cac6361
Rob <rob.nard@gmail.com>
2017-04-06 16:18:05 -0400

    Various Changes

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,20 +1,37 @@
 (function () {
-    function scrollToTop() {
+    function showButton() {
 
         var scrollButton = document.getElementById("scroll_top"),
         pageTop = document.getElementById("top"),
         minYShowButton = 503,
         minXShowButton = 767,
         pageYPosition = window.pageYOffset,
-        pageWidth = window.innerWidth;
+        pageWidth = window.innerWidth,
+        state = false;
 
-        if ((pageYPosition >= minYShowButton) && (pageWidth >= minXShowButton)) {
+        if ((pageYPosition >= minYShowButton) && state == false) {
+            state = true;
+        }
+        else if ((pageYPosition <= minYShowButton) && state == true) {
+            state = false;
+        }
+
+        if (state == true) {
+            scrollButton.classList.remove("fade_out");
             scrollButton.classList.add("fade_in");
         }
-        /*else {
+
+        else if (state == false) {
+            scrollButton.classList.remove("fade_in");
             scrollButton.classList.add("fade_out");
-        }*/
+        }
     }
-    /*scrollButton.addEventListener("click", scrollToTop, false);*/
-    window.addEventListener("scroll", scrollToTop, false);
+    window.addEventListener("scroll", showButton, false);
 })();
+
+(function () {
+    function fath() {
+        alert("hey");
+    }
+    document.getElementById("scroll_top").addEventListener("click", fath, false);
+});
